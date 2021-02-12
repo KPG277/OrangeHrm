999ede56e16117f4f062d37fb94b4cc0221ded30
Pooja <Pooja@LAPTOP-QB7QI0P1>
2021-02-12 13:43:09 +0530

    first commit

---
+++ jarprojects/src/Test.java
@@ -0,0 +1,13 @@
+
+public class Test {
+public static void main(String[] args) {
+	Student s=new Student();
+	s.sid="1234A";
+	s.sname="Priynka";
+	s.Study();
+	Employee e=new Employee();
+	e.empno="E1234";
+	e.empname="Pooja";
+	e.Work();
+}
+}
